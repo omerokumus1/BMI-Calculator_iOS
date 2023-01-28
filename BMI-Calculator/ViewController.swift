fa8f951ab0a4652dac6b4e7b05b4cbdedc0b6d21
omerokumus <omerokumus3@gmail.com>
2023-01-28 12:31:04 +0300

    Initial Commit

---
+++ BMI-Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMI-Calculator
+//
+//  Created by Ömer Faruk Okumuş on 28.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
