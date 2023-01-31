fb2e70ed83f2e6027a702e19dc65f36c0510b6de
omerokumus <omerokumus3@gmail.com>
2023-01-31 11:17:42 +0300

    ViewController logic is done, second screen logic is added

--- BMI-Calculator/Controllers/ViewController.swift
+++ BMI-Calculator/Controllers/ViewController.swift
@@ -9,11 +9,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightSlider: UISlider!
+    
+    private var bmiCalculator = BmiCalculator()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBAction func heightSliderChanged(_ sender: UISlider) {
+        heightLabel.text = String(format: "%.2f", sender.value) + "m"
+    }
+    
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+        weightLabel.text = String(format: "%.1f", sender.value) + "kg"
+    }
+    
+    @IBAction func calculateButtonPressed(_ sender: UIButton) {
+        bmiCalculator.calculateBmi(heightSlider.value, weightSlider.value)
+        self.performSegue(withIdentifier: "goToResult", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "goToResult" {
+            let viewController = segue.destination as! ResultViewController
+            viewController.bmi = bmiCalculator.getBmi()
+        }
+    }
+    
 }
 
